f80d48d525834f8405846c032789ebb7d3030639
Troy <troyd9216@gmail.com>
2021-12-06 15:22:11 -0600

    added tailwind setup

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p class="text-red-600 text-xl">{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
