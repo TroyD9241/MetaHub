6fe0480605949b86e18d73ba0352cfe8f605c873
Troy <troyd9216@gmail.com>
2021-12-14 23:17:22 -0600

    fixed react but

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App/App';
+import App from "./App"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
