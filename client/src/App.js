ddc21156434399484296e0f7fa1ad25ce46e8fbc
Troy <troyd9216@gmail.com>
2021-12-22 11:56:32 -0600

    made progress on nabar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 // The main component in React and acts as a container for all other components
 
 import React from "react";
+import Navbar from "./layouts/Navbar";
+
 function App() {
 
   return (
-    <div>
-      Whatver
-    </div>
+    <div className="text-">
+      <Navbar/>    </div>
   );
 }
 
---
+++ client/src/layouts/LoginModal.jsx
@@ -0,0 +1,5 @@
+const LoginModal = () => {
+    return (
+        <></>
+    )
+}
